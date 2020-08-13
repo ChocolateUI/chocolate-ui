02984943ca4b9c170db9e95aa857897bb244bc85
fikyair <shiming_xsm@163.com>
2020-08-14 00:09:12 +0800

    [update] 修复autoComponent样式

--- src/App.tsx
+++ src/App.tsx
@@ -14,23 +14,41 @@ const App: React.FC = () => {
     // const handleFetch = (query: string) => {
     //     return lakersWithNumber.filter(player => player.value.includes(query))
     // }
-    const handleFetch = (query: string) => {
-        return fetch(`https://api.github.com/search/users?q=${query}`)
-            .then(res => res.json())
-            .then(({ items }) => {
-                console.log(items)
-                return items.slice(0, 10).map((item: any) => ({ value: item.login, ...item }))
-            })
-    }
+    // const handleFetch = (query: string) => {
+    //     return fetch(`https://api.github.com/search/users?q=${query}`)
+    //         .then(res => res.json())
+    //         .then(({ items }) => {
+    //             console.log(items)
+    //             return items.slice(0, 10).map((item: any) => ({ value: item.login, ...item }))
+    //         })
+    // }
 
-    const renderOption = (item: DataSourceType) => {
-        const itemWithGithub = item as DataSourceType<GithubUserProps>
-        return (
-            <>
-                <h2>Name: {itemWithGithub.value}</h2>
-                <p>url: {itemWithGithub.url}</p>
-            </>
-        )
+    // const renderOption = (item: DataSourceType) => {
+    //     const itemWithGithub = item as DataSourceType<GithubUserProps>
+    //     return (
+    //         <>
+    //             <h2>Name: {itemWithGithub.value}</h2>
+    //             <p>url: {itemWithGithub.url}</p>
+    //         </>
+    //     )
+    // }
+    const lakersWithNumber = [
+        { value: 'bradley', number: 11 },
+        { value: 'pope', number: 1 },
+        { value: 'caruso', number: 4 },
+        { value: 'cook', number: 2 },
+        { value: 'cousins', number: 15 },
+        { value: 'james', number: 23 },
+        { value: 'AD', number: 3 },
+        { value: 'green', number: 14 },
+        { value: 'howard', number: 39 },
+        { value: 'kuzma', number: 0 },
+    ]
+    // const handleFetch = (query: string) => {
+    //   return lakers.filter(name => name.includes(query)).map(name => ({value: name}))
+    // }
+    const handleFetch = (query: string) => {
+        return lakersWithNumber.filter(player => player.value.includes(query))
     }
     return (
         <div className="App">
@@ -41,7 +59,7 @@ const App: React.FC = () => {
                         width={400}
                         size="sm"
                         onSelect={(item) => { console.log('===>', item) }}
-                        renderOption={renderOption}
+                    // renderOption={renderOption}
                     />
                 </div>
             </header>
