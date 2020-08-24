2e1058e01785d6e52f012167aebea48580d52759
fikyair <shiming_xsm@163.com>
2020-08-24 09:22:27 +0800

    [Fixbug].d.ts

---
+++ react-app-dev.d.ts
@@ -0,0 +1,10 @@
+declare module '*.md' {
+  const value: string;
+  export default value;
+}
+
+declare module 'react-lowlight'
+
+declare module 'highlight.js/lib/languages/javascript'
+
+declare module 'highlight.js/lib/languages/typescript'
