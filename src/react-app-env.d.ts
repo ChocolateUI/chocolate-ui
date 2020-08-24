0496a6809b488632e49554afe625bd12d729ecf8
fikyair <shiming_xsm@163.com>
2020-08-24 09:24:37 +0800

    [fixbug].d.ts

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,10 @@
-/// <reference types="react-scripts" />
+declare module '*.md' {
+  const value: string;
+  export default value;
+}
+
+declare module 'react-lowlight'
+
+declare module 'highlight.js/lib/languages/javascript'
+
+declare module 'highlight.js/lib/languages/typescript'
