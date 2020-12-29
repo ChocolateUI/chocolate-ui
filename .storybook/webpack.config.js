13788877eb1b85b7c38e64dbfb9af3ab7abeed63
fikyair <shiming_xsm@163.com>
2020-12-29 14:20:18 +0800

    feat: 升级 storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -25,11 +25,11 @@ module.exports = ({
         }
       ]
     }
-    //  ,{
-    //     test: /.(scss|sass)$/,
-    //     loaders: ["style-loader", "css-loader", "sass-loader"],
-    //     include: path.resolve(__dirname, "../")
-    //   }
+    ,{
+        test: /.(scss|sass)$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"],
+        include: path.resolve(__dirname, "../")
+    }
   );
 
   config.resolve.extensions.push(".ts", ".tsx", ".md");
