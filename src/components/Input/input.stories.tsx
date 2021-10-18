00ce12b7c3e49d621c97edc47b8dff2e0ade1c43
fikyair <shiming_xsm@163.com>
2021-10-18 23:49:45 +0800

    refactor: 优化 Input  展示

--- src/components/Input/input.stories.tsx
+++ src/components/Input/input.stories.tsx
@@ -1,74 +1,73 @@
-import React from 'react'
-import { Story, Meta } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import Input, { InputProps } from './input'
-import InputDoc from './input-doc.mdx'
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+import Input, { InputProps } from "./input";
+import InputDoc from "./input-doc.mdx";
+import Card from "../Card/card";
 
-const BaseInput = (props: InputProps) => {
-  const { placeholder, size, disabled, prepend, append, defaultValue } = props;
+const BaseInput = () => {
+  const commonCss = { width: 300, marginBottom: 20 };
+  const cardCss = { margin: "20px 20px 0 0" };
   return (
-    <Input
-        size={size}
-        placeholder={placeholder}
-        disabled = {disabled}
-        prepend={prepend}
-        append={append}
-        defaultValue={defaultValue}
-        onChange={action('changed')}
-        style={{ width: 300  }}
-    />
-  )
-}
+    <div style={{ display: "flex", width: 1024, flexWrap: "wrap" }}>
+      <Card title="基本使用" style={cardCss} shadow>
+        <Input placeholder="Basic Usage" style={commonCss} />
+      </Card>
+      <Card title="不同大小" style={cardCss} shadow>
+        <Input size="sm" placeholder="Basic Usage" style={commonCss} />
+        <Input size="lg" placeholder="Basic Usage" style={commonCss} />
+      </Card>
+      <Card title="前置/后置标签" style={cardCss} shadow>
+        <Input prepend="https://" placeholder="Basic Usage" style={commonCss} />
+        <Input append=".com" placeholder="Basic Usage" style={commonCss} />
+      </Card>
+      <Card title="有默认值" style={cardCss} shadow>
+        <Input
+          size="sm"
+          placeholder="Basic Usage"
+          defaultValue="defaultValue"
+          style={commonCss}
+        />
+      </Card>
+      <Card title="不可用" style={cardCss} shadow>
+        <Input
+          disabled
+          size="sm"
+          placeholder="Basic Usage"
+          defaultValue="defaultValue"
+          style={commonCss}
+        />
+      </Card>
+    </div>
+  );
+};
 
 export default {
   component: Input,
-  title: 'Input',
+  title: "Input",
   parameters: {
     docs: {
       page: InputDoc,
       source: {
-        type: 'code'
-      }
+        type: "code",
+      },
     },
     controls: {
-      include: ['disabled', 'size', 'placeholder']
-    }
-  }
+      include: ["disabled", "size", "placeholder"],
+    },
+  },
 } as Meta;
 
-
-const _default: Story<InputProps> = (args) => <BaseInput {...args} />;
+const _default: Story<InputProps> = () => <BaseInput />;
 
 // 默认
-export const Default = _default.bind({});
+export const Primary = _default.bind({});
 
-Default.args = {
+Primary.args = {
   disabled: false,
-  size: 'sm',
+  size: "sm",
   icon: undefined,
-  placeholder: 'placeholder',
-  prepend: '',
-  append: '',
-  onChange: () => {}
-}
-
-export const WithPrepend = _default.bind({});
-
-WithPrepend.args = {
-  ...Default.args,
-  prepend: 'https://'
-}
-
-export const WidthAppend = _default.bind({});
-
-WidthAppend.args = {
-  ...Default.args,
-  append: '.com'
-}
-
-export const DefaultValue = _default.bind({});
-
-DefaultValue.args = {
-  ...Default.args,
-  defaultValue: 'you are so clever'
-}
+  placeholder: "placeholder",
+  prepend: "",
+  append: "",
+  onChange: () => {},
+};
