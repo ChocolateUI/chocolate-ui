13a8726ca7f87b68c96a309d4c76e3601ebcb15b
fikyair <shiming_xsm@163.com>
2021-10-18 21:37:53 +0800

    refactor: 优化 Button  展示

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -1,26 +1,106 @@
-import React from 'react'
-import { Story, Meta } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import Button, { BaseButtonProps } from './button'
-import ButtonDoc from './button-doc.mdx'
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+import Button, { BaseButtonProps } from "./button";
+import ButtonDoc from "./button-doc.mdx";
+import Card from "../Card/card";
 
-const BaseButton = (props: BaseButtonProps) => {
-  const { disabled, size, className, btnType, href } = props;
+const BaseButton = () => {
+  const commonCss = { marginBottom: 20, marginRight: 20 };
+  const cardCss = { margin: "20px 20px 0 0" };
   return (
-    <Button
-      size={size}
-      href={href}
-      disabled={disabled}
-      className={className}
-      btnType={btnType}
-      onClick={action('onClick')}
-    > button </Button>
-  )
-}
+    <div style={{ display: "flex", width: 1024, flexWrap: "wrap" }}>
+      <Card title="基础使用" style={cardCss} shadow>
+        <Button btnType="default" style={commonCss}>
+          Default
+        </Button>
+        <Button btnType="primary" style={commonCss}>
+          Primary
+        </Button>
+        <Button btnType="danger" style={commonCss}>
+          Danger
+        </Button>
+        <div
+          style={{
+            backgroundColor: "#e6ecf1",
+            height: 40,
+            borderRadius: 4,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginRight: 20,
+          }}
+        >
+          <Button btnType="ghost">Ghost</Button>
+        </div>
+        <Button btnType="link" style={commonCss}>
+          Link
+        </Button>
+      </Card>
+      <Card title="不同大小" style={cardCss} shadow>
+        <Button size="sm" btnType="default" style={commonCss}>
+          Small Default
+        </Button>
+        <Button size="lg" btnType="primary" style={commonCss}>
+          Large Primary
+        </Button>
+        <Button size="lg" btnType="danger" style={commonCss}>
+          Large Danger
+        </Button>
+        <div
+          style={{
+            backgroundColor: "#6777ef",
+            height: 50,
+            borderRadius: 4,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginRight: 20,
+          }}
+        >
+          <div>
+            <Button size="lg" btnType="ghost">
+              Large Ghost
+            </Button>
+          </div>
+        </div>
+        <Button size="lg" btnType="link" style={commonCss}>
+          Large Link
+        </Button>
+      </Card>
+      <Card title="Icon 按钮" style={cardCss} shadow>
+        <Button icon="check" btnType="default" style={commonCss}>
+          Default
+        </Button>
+        <Button icon="battery-half" btnType="primary" style={commonCss}>
+          Primary
+        </Button>
+        <Button icon="bolt" btnType="danger" style={commonCss}>
+          Danger
+        </Button>
+        <div
+          style={{
+            backgroundColor: "#e6ecf1",
+            height: 40,
+            borderRadius: 4,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginRight: 20,
+          }}
+        >
+          <Button icon="caret-square-right" btnType="ghost">Ghost</Button>
+        </div>
+        <Button btnType="link" style={commonCss}>
+          Link
+        </Button>
+      </Card>
+    </div>
+  );
+};
 
 export default {
   component: Button,
-  title: 'Button',
+  title: "Button 按钮",
   decorators: [
     (Story) => (
       <div>
@@ -30,67 +110,35 @@ export default {
   ],
   argTypes: {
     size: {
-      options: ['sm', 'lg'],
-      control: { type: 'select' }
+      options: ["sm", "lg"],
+      control: { type: "select" },
     },
     href: {
-      options: ['http://www.baidu.com', 'http://www.google.com'],
-      control: { type: 'radio' }
-    }
+      options: ["http://www.baidu.com", "http://www.google.com"],
+      control: { type: "radio" },
+    },
   },
   parameters: {
     docs: {
       page: ButtonDoc,
       source: {
-        type: 'code'
-      }
+        type: "code",
+      },
     },
-    layout: 'centered',
-    controls: { exclude: ['className', 'btnType'] }
+    layout: "centered",
+    controls: { exclude: ["className", "btnType"] },
   },
 } as Meta;
 
 // _default
-const _default: Story<BaseButtonProps> = (args) => <BaseButton {...args} />;
+const _default: Story<BaseButtonProps> = () => <BaseButton />;
 
-export const Default = _default.bind({});
-
-Default.args = {
-  className: '',
-  disabled: false,
-  size: 'sm',
-  btnType: 'default',
-  href: 'http://www.baidu.com',
-};
-
-// _primary
 export const Primary = _default.bind({});
 
 Primary.args = {
- ...Default.args,
- btnType: 'primary'
-};
-
-// _danger
-export const Danger = _default.bind({});
-
-Danger.args = {
- ...Default.args,
- btnType: 'danger'
-};
-
-// _link
-export const Ghost = _default.bind({});
-
-Ghost.args = {
- ...Default.args,
- btnType: 'ghost'
-};
-
-// _link
-export const Link = _default.bind({});
-
-Link.args = {
- ...Default.args,
- btnType: 'link'
+  className: "",
+  disabled: false,
+  size: "sm",
+  btnType: "default",
+  href: "http://www.baidu.com",
 };
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,93 +1,99 @@
-import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react'
-import classNames from 'classnames'
+import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from "react";
+import classNames from "classnames";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import Icon, { ThemeProps } from "../Icon/icon";
 
-export type ButtonSize = 'lg' | 'sm'
-export type ButtonType = 'primary' | 'default' | 'danger' | 'link' | 'ghost'
+export type ButtonSize = "lg" | "sm";
+export type ButtonType = "primary" | "default" | "danger" | "link" | "ghost";
 
 export interface BaseButtonProps {
-    /**
-     * 设置按钮额外的样式
-     */
-    className?: string;
-    /**
-     * 设置按钮是否可用
-     */
-    disabled?: boolean;
-    /**
-     * 设置按钮大小
-     */
-    size?: ButtonSize;
-    /**
-     * 设置按钮类型
-     */
-    btnType?: ButtonType;
-    children: React.ReactNode;
-    /**
-     * 设置 link 类型按钮的跳转链接
-     */
-    href?: string;
+  /**
+   * 设置按钮额外的样式
+   */
+  className?: string;
+  /**
+   * 设置按钮是否可用
+   */
+  disabled?: boolean;
+  /**
+   * 设置按钮大小
+   */
+  size?: ButtonSize;
+  /**
+   * 设置按钮类型
+   */
+  btnType?: ButtonType;
+  children: React.ReactNode;
+  /**
+   * 设置 link 类型按钮的跳转链接
+   */
+  href?: string;
+  /**
+   * 添加图标到标题后
+   */
+  icon?: IconProp;
+  /**
+   * 图标主题
+   */
+  theme?: ThemeProps;
 }
 
-type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>
-type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>
-export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>;
+type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>;
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
 // export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
 
 /**
  * 页面中最常用的的按钮元素，适合于完成特定的交互
  * ### 引用方法
- * 
+ *
  * ~~~js
  * import { Button } from 'chocolate-ui'
  * ~~~
  */
 export const Button: FC<ButtonProps> = (props) => {
-    const {
-        btnType,
-        className,
-        disabled,
-        size,
-        children,
-        href,
-        ...restProps
-    } = props;
+  const {
+    btnType,
+    className,
+    disabled,
+    size,
+    children,
+    href,
+    icon,
+    theme,
+    ...restProps
+  } = props;
 
-    const classes = classNames('btn', className, {
-        [`btn-${btnType}`]: btnType,
-        [`btn-${size}`]: size,
-        'disabled': (btnType === 'link') && disabled,
-    })
+  const classes = classNames("btn", className, {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    disabled: btnType === "link" && disabled,
+  });
 
-    if (btnType === 'link' && href) {
-        return (
-            <a
-                className={classes}
-                href={href}
-                {...restProps}
-            >
-                {children}
-            </a>
-        )
-    } else {
-        return (
-            <button
-                className={classes}
-                disabled={disabled}
-                {...restProps}
-            >
-                {children}
-            </button>
-        )
-    }
-}
-
+  if (btnType === "link" && href) {
+    return (
+      <a className={classes} href={href} {...restProps}>
+        {children}
+      </a>
+    );
+  } else {
+    return (
+      <button className={classes} disabled={disabled} {...restProps}>
+        {icon ? (
+          <Icon icon={icon} theme={theme} style={{ marginRight: 5 }} />
+        ) : null}
+        {children}
+      </button>
+    );
+  }
+};
 
 Button.defaultProps = {
-  className: '',
+  className: "",
   disabled: false,
-  size: 'sm',
-  btnType: 'default',
-  href: 'http://www.baidu.com',
-}
+  size: "sm",
+  btnType: "default",
+  href: "http://www.baidu.com",
+};
 
 export default Button;
