7f6b06eacf353837868e3b771478ca4183cc0f58
fikyair <shiming_xsm@163.com>
2021-10-30 14:30:19 +0800

    fix: bugs

--- src/components/Alerts/__tests__/alert.test.tsx
+++ src/components/Alerts/__tests__/alert.test.tsx
@@ -1,18 +1,8 @@
 import React from 'react'
-import { render, fireEvent, waitFor } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { Alert } from "../alert";
 
 describe('test Alert Component', ()=>{
-  it('should hidden when close Alert', async()=>{
-    const wrapper = render(<Alert message="note" closable/>)
-    const testContainer = wrapper.container.querySelector('.alert-close-icon') as HTMLElement
-    // const testAlert = wrapper.container.querySelector('.alert-wrapper') as HTMLElement
-    fireEvent.click(testContainer)
-    await waitFor(() => {
-      // expect(testAlert).not.toBeVisible()
-    })
-  })
-
   it('should render correct Alert based on closable props', () => {
     const wrapper = render(<Alert message="note" closable/>)
     const testContainer = wrapper.container.querySelector('.alert-wrapper') as HTMLElement
